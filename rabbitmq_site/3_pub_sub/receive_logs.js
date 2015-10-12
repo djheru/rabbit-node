e2eb2bc8ca74f971b63320f20da4fabbc07fb8e5
Philip Damra <djheru@gmail.com>
2015-10-11 23:36:37 -0400

    basic pubsub

---
+++ rabbitmq_site/3_pub_sub/receive_logs.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function (err, conn) {
+
+  console.log('connect complete');
+  if (err) {
+    console.log('error: ', err);
+    process.exit(1);
+  }
+
+  conn.createChannel(function (err, ch) {
+    console.log('create channel complete');
+    if (err) {
+      console.log('error: ', err);
+      process.exit(1);
+    }
+
+    var ex = 'logs';
+
+    ch.assertExchange(ex, 'fanout', {durable: false});
+
+    //declare the queue in case it not exist
+    ch.assertQueue('', {exclusive: true}, function (err, q) {
+      console.log(' [*] Waiting for messages in %s. To exit, press CTRL+C', q);
+      q.bindQueue(q.queue, ex, '');
+      ch.consume(q, function (msg) {
+        //mock a time-consuming task by appending '.'s
+        var secs = msg.content.toString().split('.').length;
+        console.log(' [*] Received %s', msg.content.toString());
+      }, {noAck: false});
+    });
+
+  });
+
+});
